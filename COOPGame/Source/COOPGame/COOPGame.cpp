ca100c19db80ff52a9cf4bd7baa45f4095ee0984
Daroi\REZA <reza.darooei@gmail.com>
2018-05-15 00:20:24 +0430

    COOP_initial

---
+++ COOPGame/Source/COOPGame/COOPGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "COOPGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, COOPGame, "COOPGame" );
