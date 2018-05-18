5fa6126cfc9f5ea0d229f0d81b3210a25986b0d5
Daroi\REZA <reza.darooei@gmail.com>
2018-05-18 20:21:57 +0430

    COOP_Create Weapon Class

---
+++ COOPGame/Source/COOPGame/Private/SWeapon.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SWeapon.h"
+
+
+// Sets default values
+ASWeapon::ASWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>("Component");
+	RootComponent = MeshComp;
+}
+
+// Called when the game starts or when spawned
+void ASWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ COOPGame/Source/COOPGame/Public/SWeapon.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SWeapon.generated.h"
+class USkeletalMeshComponent;
+UCLASS()
+class COOPGAME_API ASWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASWeapon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Component")
+	USkeletalMeshComponent* MeshComp;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
