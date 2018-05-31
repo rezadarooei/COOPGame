23fbf1f08e4a64e0420464f2e55d4f3fbf17703c
Daroi\REZA <reza.darooei@gmail.com>
2018-06-01 00:28:34 +0430

    COOP_Create Health Component

---
+++ COOPGame/Source/COOPGame/Private/Components/SHealthComponent.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SHealthComponent.h"
+
+
+// Sets default values for this component's properties
+USHealthComponent::USHealthComponent()
+{
+	Health = 100.f;
+}
+
+
+// Called when the game starts
+void USHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+
